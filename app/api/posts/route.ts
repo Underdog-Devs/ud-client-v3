7a91aa377d42be67c41cf0012cf8e2767ab3afed
William McQuaid <whmcquaid@gmail.com>
2023-08-19 18:44:02 -0700

    Add ordering to blog fetch

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,20 +1,19 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
-import { cookies } from 'next/headers'
+import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
-      const { searchParams } = new URL(request.url);
-      const supabase = createRouteHandlerClient({ cookies })
-      const from = Number(searchParams.get("page"));
-
-      const { data: pages } = await supabase
-            .from('posts')
-            .select(`
-                  *,
-                  authors ( name )
-            `)
-            .range(from || 0, from + 6)
-      return NextResponse.json({
-            items: pages
-      });
+  const { searchParams } = new URL(request.url);
+  const supabase = createRouteHandlerClient({ cookies });
+  const from = Number(searchParams.get("page") || 0);
+  console.log(from, "FROM");
+  console.log(searchParams.get("page"), "PAGE");
+  const { data: pages } = await supabase
+    .from("posts")
+    .select()
+    .order("created_at", { ascending: false })
+    .range(from, from + 6);
+  return NextResponse.json({
+    items: pages,
+  });
 }
