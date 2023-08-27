a729ee08595d9f6a5c2115f82ba81f6bb57e7d0b
Al <al@Als-MacBook-Pro.local>
2023-08-27 11:24:02 -0700

    Refactored blog posts page fetching

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,15 +2,14 @@ import "./index.scss";
 import { BlogPosts } from "../../components/blog/BlogPosts";
 import type { Post } from "@/app/types/blog";
 
-async function fetchPosts(): Promise<Post[]> {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_HOSTNAME}/api/posts`, {
-    next: { revalidate: 3600 },
-  });
-  const data = await response.json();
-  return data.items;
-}
-
 export default async function PostsPage() {
+  async function fetchPosts(): Promise<Post[]> {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_HOSTNAME}/api/posts`, {
+      next: { revalidate: 3600 },
+    });
+    const data = await response.json();
+    return data.items;
+  }
   const items = await fetchPosts();
   return <BlogPosts initialPosts={items} />;
 }
