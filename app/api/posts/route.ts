8914d3f870dc9714c6eeab04f6bc03d6a278ac43
Al <al@Als-MacBook-Pro.local>
2023-08-27 14:16:19 -0700

    Changed API url

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -6,8 +6,7 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const supabase = createServerActionClient({ cookies })
   const from = Number(searchParams.get("page") || 0);
-  console.log(from, "FROM");
-  console.log(searchParams.get("page"), "PAGE");
+  
   const { data: pages } = await supabase
     .from("posts")
     .select()
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import { BlogPosts } from "../../components/blog/BlogPosts";
 import type { Post } from "@/app/types/blog";
 
 async function fetchPosts(): Promise<Post[]> {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_HOSTNAME}/api/posts`, {
+  const response = await fetch(`/api/posts`, {
     next: { revalidate: 3600 },
   });
   const data = await response.json();
@@ -12,5 +12,7 @@ async function fetchPosts(): Promise<Post[]> {
 
 export default async function PostsPage() {
   const items = await fetchPosts();
-  return <BlogPosts initialPosts={items} />;
+  if (items) {
+    return <BlogPosts initialPosts={items} />;
+  }
 }
