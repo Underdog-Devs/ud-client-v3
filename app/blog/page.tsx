98971c3dad7687c6e22f8b0f071d39c1e5ff9f48
Al <al@Als-MacBook-Pro.local>
2023-08-27 14:48:14 -0700

    Fix fetch

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,12 @@ import { BlogPosts } from "../../components/blog/BlogPosts";
 import type { Post } from "@/app/types/blog";
 
 async function fetchPosts(): Promise<Post[]> {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_HOSTNAME}/api/posts`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_HOSTNAME}/api/posts`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   const data = await response.json();
   return data.items;
 }
