9451587c6f73769ec7712a57fe5a906bf1bbc5ab
Al <al@Als-MacBook-Pro.local>
2023-08-27 14:27:13 -0700

    ISR disabled

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,12 +1,13 @@
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const supabase = createServerActionClient({ cookies })
+  const supabase = createRouteHandlerClient({ cookies });
   const from = Number(searchParams.get("page") || 0);
-  
+
   const { data: pages } = await supabase
     .from("posts")
     .select()
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,9 +3,7 @@ import { BlogPosts } from "../../components/blog/BlogPosts";
 import type { Post } from "@/app/types/blog";
 
 async function fetchPosts(): Promise<Post[]> {
-  const response = await fetch(`/api/posts`, {
-    next: { revalidate: 3600 },
-  });
+  const response = await fetch(`${process.env.NEXT_PUBLIC_HOSTNAME}/api/posts`);
   const data = await response.json();
   return data.items;
 }
