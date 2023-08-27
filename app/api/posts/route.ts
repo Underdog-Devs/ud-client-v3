9d56276f3b62a568bb79871742a9326f3defbc33
Al <al@Als-MacBook-Pro.local>
2023-08-27 11:35:01 -0700

    Server side supabase action fix

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,10 +1,10 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const supabase = createRouteHandlerClient({ cookies });
+  const supabase = createServerActionClient({ cookies })
   const from = Number(searchParams.get("page") || 0);
   console.log(from, "FROM");
   console.log(searchParams.get("page"), "PAGE");
