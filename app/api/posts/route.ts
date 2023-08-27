30f4cfb40161417a439e89291da8676e5d10c288
Al <al@Als-MacBook-Pro.local>
2023-08-27 15:34:31 -0700

    force-dynamic enabled

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const supabase = createRouteHandlerClient({ cookies });
