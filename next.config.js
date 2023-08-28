833c27261244e92cb1f5236d4a2ab4e51aff32bb
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-28 15:31:34 -0500

    Removed experimental feature flag

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'ud-media.s3.us-east-2.amazonaws.com',
-        port: '',
+        protocol: "https",
+        hostname: "ud-media.s3.us-east-2.amazonaws.com",
+        port: "",
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
