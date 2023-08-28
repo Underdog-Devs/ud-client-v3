f26b8abb5c8f71b93cc99d01f351199deb4f91e1
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-28 13:54:45 -0500

    Added error handling and simplified data

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,21 +1,27 @@
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export const dynamic = 'force-dynamic'
+// good for testing, not for production
+export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const supabase = createRouteHandlerClient({ cookies });
   const from = Number(searchParams.get("page") || 0);
 
-  const { data: pages } = await supabase
+  // pull posts from supabase
+  const { data, error } = await supabase
     .from("posts")
     .select()
     .order("created_at", { ascending: false })
     .range(from, from + 6);
+
+  if (error) {
+    return NextResponse.json({ items: [] });
+  }
+
   return NextResponse.json({
-    items: pages,
+    items: data,
   });
 }
