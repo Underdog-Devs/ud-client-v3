54f9e1a00a5145eeb12dcc44bf638072e4b87323
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-28 15:31:48 -0500

    Added revalidation in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 // styles
-import Layout from '../components/Layout';
-import './styles/index.scss';
+import Layout from "../components/Layout";
+import "./styles/index.scss";
+
+export const revalidate = 0;
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body>
-				<Layout>
-					{children}
-				</Layout>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <body>
+        <Layout>{children}</Layout>
+      </body>
+    </html>
+  );
 }
