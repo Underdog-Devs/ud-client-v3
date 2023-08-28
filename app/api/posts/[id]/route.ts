9ea4219bc313e04bb3de048e2732229adbc606e3
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-28 15:01:16 -0500

    Added api route for fetching single posts

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+// when in development, force dynamic SSR
+export const dynamic =
+  process.env.NODE_ENV === "development" ? "force-dynamic" : "auto";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const supabase = createRouteHandlerClient({ cookies });
+
+  // pull posts from supabase
+  const { data, error } = await supabase
+    .from("posts")
+    .select()
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    return NextResponse.json({ post: {} });
+  }
+
+  return NextResponse.json({
+    post: data,
+  });
+}
