81bb48e7f91ae877f35c4f735eec444eb7cc93a1
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-30 10:27:54 -0500

    Remove forced update to enable revalidation

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,9 +2,6 @@ import "./index.scss";
 import { BlogPosts } from "../../components/blog/BlogPosts";
 import type { Post } from "@/app/types/blog";
 
-// when in development, force dynamic SSR
-export const dynamic = "force-dynamic";
-
 async function fetchPosts(): Promise<Post[]> {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_HOSTNAME}/api/posts`,
