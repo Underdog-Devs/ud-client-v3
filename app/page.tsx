0fd94c7f99ebfafd11398e6500e2358f4ec4f47b
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-30 12:54:22 -0500

    Removed testimonials from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import type { NextPage } from 'next';
-import Mentor from '../components/landing/mentor';
-import Recidivism from '../components/landing/recidivism';
-import Mentee from '../components/landing/mentee';
-import SocialMedia from '../components/landing/socialMedia';
-import GetInvolved from '../components/landing/getInvolved';
-import Hero from '../components/landing/hero';
-import Testimonials from '../components/landing/testimonials/Testimonials';
+import React from "react";
+import type { NextPage } from "next";
+import Mentor from "../components/landing/mentor";
+import Recidivism from "../components/landing/recidivism";
+import Mentee from "../components/landing/mentee";
+import SocialMedia from "../components/landing/socialMedia";
+import GetInvolved from "../components/landing/getInvolved";
+import Hero from "../components/landing/hero";
+// import Testimonials from "../components/landing/testimonials/Testimonials";
 
 const Home: NextPage = () => (
-	<>
-		<div>
-			<Hero />
-			<Recidivism />
-			<Mentee />
-			<Mentor />
-			<Testimonials />
-			<SocialMedia />
-			<GetInvolved />
-		</div>
-	</>
+  <>
+    <div>
+      <Hero />
+      <Recidivism />
+      <Mentee />
+      <Mentor />
+      {/* <Testimonials /> */}
+      <SocialMedia />
+      <GetInvolved />
+    </div>
+  </>
 );
 
 export default Home;
