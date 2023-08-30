13505df472f29bab4dd73b826612c5388efdc2fa
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-30 15:38:23 -0500

    Converted to functional component

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,20 @@
 import React from "react";
-import type { NextPage } from "next";
 import Mentor from "../components/landing/mentor";
 import Recidivism from "../components/landing/recidivism";
 import Mentee from "../components/landing/mentee";
 import SocialMedia from "../components/landing/socialMedia";
 import GetInvolved from "../components/landing/getInvolved";
 import Hero from "../components/landing/hero";
-// import Testimonials from "../components/landing/testimonials/Testimonials";
 
-const Home: NextPage = () => (
-  <>
-    <div>
+export default function Home() {
+  return (
+    <>
       <Hero />
       <Recidivism />
       <Mentee />
       <Mentor />
-      {/* <Testimonials /> */}
       <SocialMedia />
       <GetInvolved />
-    </div>
-  </>
-);
-
-export default Home;
+    </>
+  );
+}
