3f4c91e1275e8cd2a5d80d12d08422dfc58f1f2f
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-30 15:41:02 -0500

    Added caching back

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,9 +2,6 @@ import "./index.scss";
 import { BlogPosts } from "../../components/blog/BlogPosts";
 import type { Post } from "@/app/types/blog";
 
-// If this won't deploy on AWS, try to add the next line
-export const dynamic = "force-dynamic";
-
 async function fetchPosts(): Promise<Post[]> {
   // Revalidates after 15 minutes
   const response = await fetch(
