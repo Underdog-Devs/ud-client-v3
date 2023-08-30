57a59761718fe1d059b95df31e262d357f731dd0
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-30 16:17:21 -0500

    Simplified component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,20 +2,15 @@ import "./index.scss";
 import { BlogPosts } from "../../components/blog/BlogPosts";
 import type { Post } from "@/app/types/blog";
 
-async function fetchPosts(): Promise<Post[]> {
-  // Revalidates after 15 minutes
+export default async function PostsPage() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_HOSTNAME}/api/posts`,
     { next: { revalidate: 900 } }
   );
-  const data: { items: Post[] } = await response.json();
-  return data?.items || [];
-}
 
-export default async function PostsPage() {
-  const items = await fetchPosts();
-  if (items.length) {
-    return <BlogPosts initialPosts={items} />;
-  }
-  return <BlogPosts initialPosts={[]} />;
+  const data: { items: Post[] } = await response.json();
+
+  const items = data?.items || [];
+
+  return <BlogPosts initialPosts={items} />;
 }
